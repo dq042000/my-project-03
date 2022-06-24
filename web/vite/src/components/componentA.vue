672c78239ed1470395156a6035888529f5088d11
mike <dq042000@gmail.com>
2022-06-24 23:41:34 +0800

    資料綁定

---
+++ web/vite/src/components/componentA.vue
@@ -0,0 +1,15 @@
+<template>
+    <p>Hello World</p>
+    {{ data }}
+    <input type="text" v-model="text" />
+    <p>{{ text }}</p>
+</template>
+
+<script setup>
+import { ref } from "@vue/reactivity";
+
+const data = "Hello Vue";
+const text = ref("123");
+</script>
+
+<style></style>
