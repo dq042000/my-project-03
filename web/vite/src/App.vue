769a3bf1ddab6a30ce695611a7f4d03391dc1064
Mike Ci <dq042000@gmail.com>
2022-07-16 16:56:52 +0800

    test watch

--- web/vite/src/App.vue
+++ web/vite/src/App.vue
@@ -2,12 +2,14 @@
     <componentA />
     <componentB />
     <componentC />
+    <componentD />
 </template>
 
 <script setup>
 import componentA from "./components/componentA.vue";
 import componentB from "./components/componentB.vue";
 import componentC from "./components/componentC.vue";
+import componentD from "./components/componentD.vue";
 </script>
 
 <style>
