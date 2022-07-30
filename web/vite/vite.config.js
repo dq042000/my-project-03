98571690e871f1fad13d79e1e39bfb5382236d23
Mike Ci <dq042000@gmail.com>
2022-07-30 23:05:25 +0800

    調整架構

--- web/vite/vite.config.js
+++ web/vite/vite.config.js
@@ -5,6 +5,12 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
     plugins: [vue()],
     server: {
+        // required to load scripts from custom host
+        cors: true,
+
+        // we need a strict port to match on PHP side
+        // change freely, but update on PHP to match the same port
+        strictPort: true,
         host: "0.0.0.0",
         port: 3000,
     },
